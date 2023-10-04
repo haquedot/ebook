e7d9acb6c45c08ea3bb1d77de757f21ec4954f12
MD MERAJUL HAQUE <113513789+haquedot@users.noreply.github.com>
2023-10-04 22:46:21 +0530

    Add files via upload

---
+++ javascript/editor.js
@@ -0,0 +1,1 @@
+var pageEditor ={"setting":{"annoPlaying":"true","shoppingCartHTML":"false","shoppingCartOptinon":{"type":"PayPal","paypal":"","method":"POST","sandbox":"false","address":"","theme":"","body":"Hi xxx<br/>   I'm going to buy below product(s):<br/>    ${shopping}<br/>Full Name","showPrice":"true","showTime":"true"}}, "pageAnnos":[]};
